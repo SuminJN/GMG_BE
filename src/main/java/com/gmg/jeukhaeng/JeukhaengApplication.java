980e3cfc4512d3ef336206b20ab4747542cfcbbc
Sumin Jeon <wjstnals1211@gmail.com>
2025-07-09 15:03:33 +0900

    [Setup]: Spring Boot project 초기화

---
+++ src/main/java/com/gmg/jeukhaeng/JeukhaengApplication.java
@@ -0,0 +1,13 @@
+package com.gmg.jeukhaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeukhaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeukhaengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmg/jeukhaeng/JeukhaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmg.jeukhaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeukhaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c3697653856b410efbf6b7b5bcab26632d86c68b
Sumin Jeon <wjstnals1211@gmail.com>
2025-08-26 18:02:50 +0900

    feat: TripRepository에 사용자 기반 여행 조회 메서드 추가

--- src/main/java/com/gmg/jeukhaeng/trip/repository/TripRepository.java
+++ src/main/java/com/gmg/jeukhaeng/trip/repository/TripRepository.java
@@ -1,7 +1,13 @@
 package com.gmg.jeukhaeng.trip.repository;
 
 import com.gmg.jeukhaeng.trip.entity.Trip;
+import com.gmg.jeukhaeng.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TripRepository extends JpaRepository<Trip, Long> {
+    List<Trip> findByUserOrderByCreatedAtDesc(User user);
+    Optional<Trip> findByTripIdAndUser(Long tripId, User user);
 }

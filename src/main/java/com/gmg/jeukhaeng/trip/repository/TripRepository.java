086ea5cf8a55ddd5d1499f77e62f537263b3cb36
seowon <seowon1112@gmail.com>
2025-08-26 21:31:27 +0900

    feat: 다녀온 여행 목록 조회 및 생성 여행 목록 조회

--- src/main/java/com/gmg/jeukhaeng/trip/repository/TripRepository.java
+++ src/main/java/com/gmg/jeukhaeng/trip/repository/TripRepository.java
@@ -1,6 +1,7 @@
 package com.gmg.jeukhaeng.trip.repository;
 
 import com.gmg.jeukhaeng.trip.entity.Trip;
+import com.gmg.jeukhaeng.trip.entity.TripStatus;
 import com.gmg.jeukhaeng.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,8 @@ import java.util.Optional;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
     List<Trip> findByUserOrderByCreatedAtDesc(User user);
+    List<Trip> findByUserAndStatusOrderByCreatedAtDesc(User user, TripStatus status);
+
+    Optional<Trip> findByTripIdAndUserAndStatus(Long tripId, User user, TripStatus status);
     Optional<Trip> findByTripIdAndUser(Long tripId, User user);
 }

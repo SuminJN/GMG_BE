02784526096fcdb8121628c04edd208a45ac5224
Sumin Jeon <wjstnals1211@gmail.com>
2025-08-27 18:51:51 +0900

    feat: ContentRepository에 지역 코드 및 시군구 코드로 콘텐츠 조회 메서드 추가

--- src/main/java/com/gmg/jeukhaeng/content/repository/ContentRepository.java
+++ src/main/java/com/gmg/jeukhaeng/content/repository/ContentRepository.java
@@ -1,8 +1,13 @@
 package com.gmg.jeukhaeng.content.repository;
 
 import com.gmg.jeukhaeng.content.entity.Content;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ContentRepository extends JpaRepository<Content, String> {
     boolean existsByContentId(String contentId);
+    Page<Content> findByAreaCodeAndSigunguCode(String areaCode, String sigunguCode, Pageable pageable);
 }
